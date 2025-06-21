24c6a4ef382190771a049be8ee342303484ed847
MilinHD <106997937+MilinHD@users.noreply.github.com>
2025-06-21 03:15:48 -0500

    Delete mi-pagina-gemini/netlify/functions/hello.js
    No funciona mas

--- mi-pagina-gemini/netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-﻿// Contenido para el nuevo archivo: netlify/functions/hello.js
-exports.handler = async function (event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "¡Hola desde la función de Netlify! El despliegue funcionó." }),
-    };
-};
